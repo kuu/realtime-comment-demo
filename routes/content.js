e73e91a4c2f239d1d78a382cfbed89e81c5f3b97
Kuu Miyazaki <miyazaqui@gmail.com>
2016-10-31 11:33:49 +0900

    Some fixes.

--- routes/content.js
+++ routes/content.js
@@ -9,26 +9,32 @@ const print = debug('comment');
 
 const {pcode, playerBrandingId, version} = config.player;
 
+const clients = new Map();
+
 router.get('/:id', (req, res) => {
   const embedCode = req.params.id;
 
   print(`/content/${embedCode} called.`);
 
-  function onConnect(client) {
-    print(`A client connected to the room #${embedCode}`);
+  socket.io.on('connection', client => {
+    if (clients.get(client.id)) {
+      // To brevent redundant connections
+      return;
+    }
+    clients.set(client.id, client);
+    print(`A client (id=${client.id}) connected to the room #${embedCode}`);
     client.join(embedCode).on('comment', msg => {
       print(`A comment posted to the room #${msg.embedCode}`);
-      print(`comment: ${msg.comment}`);
+      print(`\tcomment: ${msg.comment}`);
       // socket.broadcast.to(embedCode).emit('comment', msg);
       socket.io.to(embedCode).emit('comment', msg);
     });
     client.on('disconnect', () => {
-      print(`A client left the room #${embedCode}`);
-      socket.io.removeListener(onConnect);
+      print(`A client (id=${client.id}) left the room #${embedCode}`);
+      clients.delete(client.id);
       client.leave(embedCode);
     });
-  }
-  socket.io.on('connection', onConnect);
+  });
 
   res.render('content', {embedCode, pcode, playerBrandingId, version});
 });
